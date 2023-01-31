c45fb9e6cb357e4d401573aa370b8142b6fb7dd3
V1kata <viktor.burboran@gmail.com>
2023-01-31 14:24:21 +0200

    session, 404 and profile pages

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('express-handlebars');
+const expressSession = require('express-session');
 const cookieParser = require('cookie-parser');
 
 // set app
@@ -11,9 +12,15 @@ app.engine('hbs', hbs.engine({
 }));
 app.set('view engine', 'hbs');
 
-// set urlencoded, coockie parser
+// set urlencoded, coockie parser, session (directly from npm)
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true }
+}));
 
 app.get('/', (req, res) => {
     res.render('home');
@@ -27,14 +34,34 @@ app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
     if (username !== 'V1kata' && password !== '123') {
-        res.status(401).end();
+        res.redirect('/404');
     }
-    
-    console.log(username, password);
+
+    const data = {
+        username,
+        privateInfo: 'some private info'
+    }
+
+    res.cookie('auth', JSON.stringify(data));
+    req.session.username = username;
+    req.session.secret = data.privateInfo;
+    res.redirect('/');
 });
 
 app.get('/profile', (req, res) => {
+    const authData = req.cookies['auth'];
 
+    if (!authData) {
+        return res.redirect('/404');
+    }
+
+    const data = JSON.parse(authData);
+
+    res.render('profile', data);
+});
+
+app.get('*', (req, res) => {
+    res.render('404');
 })
 
 app.listen(5000, () => console.log('Server is running on http://localhost:5000'));
