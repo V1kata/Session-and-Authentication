48629089dbe3fac01b325d8f4b4b72aa5b307f47
V1kata <viktor.burboran@gmail.com>
2023-01-31 13:28:39 +0200

    Login, nav and get and post on /login

--- index.js
+++ index.js
@@ -7,9 +7,28 @@ app.engine('hbs', hbs.engine({
     extname: 'hbs'
 }));
 app.set('view engine', 'hbs');
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.get('/login', (req, res) => {
+    res.render('login');
+})
+
+app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+
+    if (username !== 'V1kata' && password !== '123') {
+        res.status(401).end();
+    }
+    
+    console.log(username, password);
+});
+
+app.get('/profile', (req, res) => {
+    
+})
 
 app.listen(5000, () => console.log('Server is running on http://localhost:5000'));
